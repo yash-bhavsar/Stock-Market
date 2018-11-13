96e1095870fc787f4e5f8f5599a48c8b5814b125
yash-bhavsar <yashbhavsar5895@gmail.com>
2018-11-12 20:34:35 -0500

    Objective 1 accomplished

--- src/model/User.java
+++ src/model/User.java
@@ -1,13 +1,22 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class User {
 
   private List<Portfolio> portfolios;
 
+  public User() {
+    this.portfolios = new ArrayList<>();
+  }
+
   public List<Portfolio> getPortfolios() {
     return portfolios;
+  }
+
+  public void createPortfolio() {
+    Portfolio portfolio = new Portfolio();
+    this.portfolios.add(portfolio);
   }
 }
