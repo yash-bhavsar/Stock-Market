1034ac8374a0a8886da9ed9e6c3bc214fe2faf3b
yash-bhavsar <yashbhavsar5895@gmail.com>
2018-11-28 17:52:26 -0500

    Changes

--- test/StockMarketViewImplTest.java
+++ test/StockMarketViewImplTest.java
@@ -37,19 +37,20 @@ public class StockMarketViewImplTest {
   @Test
   public void testInvalidInput() {
     try {
-      rd = new StringReader("6 -1 1 2 AAPL 1 2018-05-15 1 3 1 5");
+      rd = new StringReader("8 -1 1 2 AAPL 1 2018-05-15 1 5 1 2018-05-15 5");
       ap = new StringBuffer();
       sv = new StockMarketViewImpl(rd, ap);
       sv.enterCommand();
-      assertEquals("\n" +
-              "Enter Choice:\n" +
+      assertEquals("\nEnter Choice:\n" +
               " 1. Create new portfolio.\n" +
               " 2. Buy Stock.\n" +
-              " 3. View Composition of a portfolio.\n" +
-              " 4. View total cost basis and evaluation of a portfolio on a particular date.\n" +
-              " 5. Quit.\n" +
+              " 3. Invest\n" +
+              " 4. Create Strategy\n" +
+              " 5. View Composition of a portfolio.\n" +
+              " 6. View total cost basis and evaluation of a portfolio on a particular date.\n" +
+              " 7. Quit.\n" +
               "\n" +
-              "\t\t\t\t\tEnter valid choice. JAI HIND!!!\n", ap.toString());
+              "\t\t\t\t\t\tEnter valid choice.\n", ap.toString());
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -61,7 +62,7 @@ public class StockMarketViewImplTest {
   @Test
   public void testPortfolioNumber() {
     try {
-      rd = new StringReader("1 -1 1 2 AAPL 1 2018-05-15 1 3 1 5");
+      rd = new StringReader("1 -1 1 2 AAPL 1 2018-05-15 1 5 1 2018-05-15 5");
       ap = new StringBuffer();
       sv = new StockMarketViewImpl(rd, ap);
       sv.enterCommand();
@@ -69,9 +70,11 @@ public class StockMarketViewImplTest {
               "Enter Choice:\n" +
               " 1. Create new portfolio.\n" +
               " 2. Buy Stock.\n" +
-              " 3. View Composition of a portfolio.\n" +
-              " 4. View total cost basis and evaluation of a portfolio on a particular date.\n" +
-              " 5. Quit.\n" +
+              " 3. Invest\n" +
+              " 4. Create Strategy\n" +
+              " 5. View Composition of a portfolio.\n" +
+              " 6. View total cost basis and evaluation of a portfolio on a particular date.\n" +
+              " 7. Quit.\n" +
               "\n" +
               "Enter portfolio number: \n" +
               "Enter valid number.\n" +
@@ -88,7 +91,7 @@ public class StockMarketViewImplTest {
   @Test
   public void testNegativeShares() {
     try {
-      rd = new StringReader("2 AAPL -1 1 2018-05-15 1");
+      rd = new StringReader("2 AAPL -1 1 2018-05-15 1 5 ");
       ap = new StringBuffer();
       sv = new StockMarketViewImpl(rd, ap);
       sv.enterCommand();
@@ -96,9 +99,11 @@ public class StockMarketViewImplTest {
               "Enter Choice:\n" +
               " 1. Create new portfolio.\n" +
               " 2. Buy Stock.\n" +
-              " 3. View Composition of a portfolio.\n" +
-              " 4. View total cost basis and evaluation of a portfolio on a particular date.\n" +
-              " 5. Quit.\n" +
+              " 3. Invest\n" +
+              " 4. Create Strategy\n" +
+              " 5. View Composition of a portfolio.\n" +
+              " 6. View total cost basis and evaluation of a portfolio on a particular date.\n" +
+              " 7. Quit.\n" +
               "\n" +
               "Enter Stock details: \n" +
               "Stock symbol(Ticker): \n" +
@@ -106,8 +111,9 @@ public class StockMarketViewImplTest {
               "Enter valid number.\n" +
               " \n" +
               "The number of shares you want to buy: \n" +
-              "The date at which you want to buy stock: \n" +
-              "Enter portfolio number: ", ap.toString());
+              "Please enter date(yyyy-mm-dd): \n" +
+              "Enter portfolio number: \n" +
+              "Enter commission amount: ", ap.toString());
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -119,7 +125,7 @@ public class StockMarketViewImplTest {
   @Test
   public void testFutureDate() {
     try {
-      rd = new StringReader("2 AAPL 2 2018-11-26 2018-11-14 1 5");
+      rd = new StringReader("2 AAPL 2 2018-12-29 2018-11-14 1 5");
       ap = new StringBuffer();
       sv = new StockMarketViewImpl(rd, ap);
       sv.enterCommand();
@@ -127,21 +133,23 @@ public class StockMarketViewImplTest {
               "Enter Choice:\n" +
               " 1. Create new portfolio.\n" +
               " 2. Buy Stock.\n" +
-              " 3. View Composition of a portfolio.\n" +
-              " 4. View total cost basis and evaluation of a portfolio on a particular date.\n" +
-              " 5. Quit.\n" +
+              " 3. Invest\n" +
+              " 4. Create Strategy\n" +
+              " 5. View Composition of a portfolio.\n" +
+              " 6. View total cost basis and evaluation of a portfolio on a particular date.\n" +
+              " 7. Quit.\n" +
               "\n" +
               "Enter Stock details: \n" +
               "Stock symbol(Ticker): \n" +
               "The number of shares you want to buy: \n" +
-              "The date at which you want to buy stock: \n" +
+              "Please enter date(yyyy-mm-dd): \n" +
               "Future dates are not valid.\n" +
               "\n" +
-              "The date at which you want to buy stock: \n" +
-              "Enter portfolio number: ", ap.toString());
+              "Please enter date(yyyy-mm-dd): \n" +
+              "Enter portfolio number: \n" +
+              "Enter commission amount: ", ap.toString());
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
-
 }
